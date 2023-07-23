fda6dd13db6f4458982c8f28b91f2d8587512bee
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-07-23 22:27:25 +0200

    Internet speed
    Files

--- charpter05/speedtest.py
+++ charpter05/speedtest.py
@@ -1,10 +1,46 @@
+#import libraries
 import flet as ft
+import speedtest
+from time import sleep
 
+#define a main function
 def main(page: ft.Page):
     
+    page.title = "Internet Speed Test"
+    page.theme_mode = "dark"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    page.window_bgcolor = "blue"
+    page.padding = 30
+    page.bgcolor = "#000000"
+    
+    #enable scroll in the page
+    page.auto_scroll = True 
+    
+    # configuring the fonts
+    page.fonts = {
+        "Rooster" : "fonts/RoosterPersonalUse-3z8d8.ttf",
+        "SourceCode" : "fonts/SourceCodePro-BlackItalic.ttf",
+        "SourceCodeBold" : "fonts/SourceCodePro-Bold.ttf"
+    }
+    
+    # make the heading of the app
+    appTitle = ft.Row(
+        controls = [
+            ft.Text("INTERNET", font_family="Rooster", style="displayLarge", color="#ff3300"),
+            ft.Text("SPEED", font_family="Rooster", style="displayLarge", color="#ffff00")
+        ]
+    )
+    
+    
+    #page components
+    page.add(
+        appTitle,
+        #getSpeedContainer,
+        ft.IconButton(icon=ft.icons.PLAY_CIRCLE_FILL_OUTLINED)
+    )
 
 
 
-
-
-ft.app(target=main)
+#executing the main function
+ft.app(target=main, assets_dir="assets")
